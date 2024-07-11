f5751f35e13cce279cf8461256d0ff3508016870
Vincenzo Ciaralli <v.ciaralli@gmail.com>
2024-07-11 18:54:00 +0200

    delete api-consumer and api-producer

--- api-consumer/src/test/java/org/dapr/ConsumerResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.dapr;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class PublishResourceIT extends PublishResourceTest {
-    // Execute the same tests but in packaged mode.
-}
--- api-producer/src/test/java/org/dapr/ProducerResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.dapr;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ProducerResourceIT extends ProducerResourceTest {
-    // Execute the same tests but in packaged mode.
-}
